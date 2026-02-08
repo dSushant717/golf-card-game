08af3d1df9be474db3b4b67db27b73d1b244cdb2
Sushant <sdahal249@gmail.com>
2026-02-07 22:21:33 -0500

    Added the Vite Server URL in .env

--- apps/web/src/socket.js
+++ apps/web/src/socket.js
@@ -3,5 +3,6 @@ import { io } from "socket.io-client";
 const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:4000";
 
 export const socket = io(URL, {
-  transports: ["websocket"]
+  transports: ["websocket", "polling"],
+  withCredentials: false,
 });
